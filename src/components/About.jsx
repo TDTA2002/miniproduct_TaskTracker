a3965b332549eb3dd020e9cc6496295c1605e8ea
TDTA2002 <tranduytuananh203@gmail.com>
2023-06-27 16:39:49 +0700

    add file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function About() {
     return (
         <div style={{textAlign:"center"}}>
-            <Link to="/">Lãi suất ngân hàng của em vừa đáo hạn nên cũng chả mua được đôi waveeeeeeeeeeeeeeeeee </Link>
+            <Link to="/"><h1>Lãi suất ngân hàng của em vừa đáo hạn nên cũng chả mua được đôi waveeeeeeeeeeeeeeeeee</h1> </Link>
         </div>
     )
 }
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,58 +1,50 @@
-import React from 'react'
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
+import React from 'react';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
 
-export default function AddTask({tasks, handleDeleteTask}) {
+export default function AddTask({ tasks, handleDeleteTask }) {
     return (
         <>
-            <Grid container style={{ marginLeft: "20px" }}>
-                <div >
-                    {tasks.length > 0 ? (
-                        tasks.map((item) => (
-                            <div
-                                key={item.id}
-                                style={{
-                                    marginLeft: "15px",
-                                    borderLeft: item.reminder ? "4px solid green" : "4px solid red",
-                                    paddingLeft: item.reminder ? "10px" : "10px",
-                                    background: "#e4e4e6",
-                                    display: "flex",
-                                    justifyContent: "space-between",
-                                    width: "430px",
-                                    marginLeft: "10px"
-                                }}
-                            >
-                                <div>
-                                    <a style={{ fontSize: "19px" }}>{item.task}</a>
-                                    <br /> <a style={{ top: '0px' }}>{item.addDayDate}</a>
-                                </div>
-                                <button
-                                    onClick={() => handleDeleteTask(item.id)}
-                                    variant="outlined"
-                                    style={{ color: "red", border: "none", background: "none", fontSize: "20px" }}
-                                >
-                                    x
-                                </button>
+            <Grid container style={{ marginLeft: '20px' }}>
+                {tasks.length > 0 ? (
+                    tasks.map((item) => (
+                        <div
+                            key={item.id}
+                            style={{
+                                borderLeft: item.reminder ? '4px solid green' : '4px solid red',
+                                paddingLeft: item.reminder ? '10px' : '10px',
+                                background: '#e4e4e6',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                width: '430px',
+                                marginLeft: '10px'
+                            }}
+                        >
+                            <div>
+                                <a style={{ fontSize: '19px' }}>{item.task}</a>
+                                <br />
+                                <a style={{ top: '0px' }}>{item.addDayDate}</a>
                             </div>
-                        ))
-                    ) : (
-                        <div style={{ textAlign: "center", padding: "10px" }}>
-                            No Task To Show
+                            <button
+                                onClick={() => handleDeleteTask(item.id)}
+                                style={{ color: 'red', border: 'none', background: 'none', fontSize: '20px' }}
+                            >
+                                x
+                            </button>
                         </div>
-                    )}
-                </div>
+                    ))
+                ) : (
+                    <div style={{ textAlign: 'center', padding: '10px' }}>No Task To Show</div>
+                )}
             </Grid>
 
-            <div>
-                MiniProject API & Asynchronous © 2023
-            </div>
+            <div>MiniProject API & Asynchronous © 2023</div>
 
             <Grid item xs>
-                <Link href="/about" variant="body2" style={{ textAlign: "center" }}>
+                <Link href="/about" variant="body2" style={{ textAlign: 'center' }}>
                     About
                 </Link>
-                <Grid item></Grid>
             </Grid>
         </>
-    )
+    );
 }
